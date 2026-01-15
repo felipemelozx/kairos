17d5d9687e075f04d494257a28c583026b6e84f9
felipemelozx <felipemelozx@gmail.com>
2026-01-14 21:11:40 -0300

    first commit

---
+++ src/test/java/com/felipemelozx/kairos/KairosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.kairos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
